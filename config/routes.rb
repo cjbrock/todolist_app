ad1b3c523ffc43fe4ffa5c2b2a1cb144964969c7
Corinna Brock <corinna.brock@gmail.com>
2012-07-16 16:11:45 -0400

    pluralized lists

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ TodolistApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'list#index'
+  root :to => 'lists#index'
 
   # See how all your routes lay out with "rake routes"
 
