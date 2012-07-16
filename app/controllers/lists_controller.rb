7fa8484e1b052e03672225c7c03af41e98598799
Corinna Brock <corinna.brock@gmail.com>
2012-07-16 16:14:56 -0400

    fixed class names

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,4 @@
-class ListController < ApplicationController
+class ListsController < ApplicationController
   def index
     @lists = List.all
 
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,4 @@
-class TaskController < ApplicationController
+class TasksController < ApplicationController
   def new
   end
 
